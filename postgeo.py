845e34ab005cce362ddd2a0f1cb888123e3590c0
Mike Stucka <stucka@github>
2015-11-11 15:57:41 -0500

    What is this I don't even

--- postgeo.py
+++ postgeo.py
@@ -66,7 +66,8 @@ def main():
                     row.append(lastlatlong)
                     put.writerow(row)
                     outputfile.flush()
-                    os.fsync()          # Force writes after each line. Should be no performance hit because geocoding
+##                    os.fsync()        # Disabled. Error after ~20 successes: fsync() takes exactly one argument (0 given)  
+                                        # Force writes after each line. Should be no performance hit because geocoding
                                         # is so slow.
                 else:
                     location = geolocator.geocode(fulladdy)
